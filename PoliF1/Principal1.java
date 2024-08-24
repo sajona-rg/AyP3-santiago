a83662db09c176119f2b362f8b66aef8075ebee9
santiago sajona <sajonasantiago2@gmail.com>
2024-08-24 11:04:53 -0500

    maqueta de nuestro polinomio forma1

---
+++ PoliF1/Principal1.java
@@ -0,0 +1,35 @@
+package PoliF1;
+
+public class Principal1
+{ 
+  public static void main(String[] args) {
+
+        PoliF1 pol1 = new PoliF1(0, -8);
+        pol1.insertarTermino(3,2);
+        pol1.insertarTermino(2, 4);
+
+       System.out.println("pol1\n"+ pol1.mostrarPolinomio());
+        
+        PoliF1 pol2 = new PoliF1(2, 4);
+        pol2.insertarTermino(2,5);
+        pol2.insertarTermino(3, 4);
+     
+        System.out.println("pol2\n"+pol2.mostrarPolinomio());
+
+        PoliF1 suma;
+
+        suma = pol1.sumar(pol2);
+
+        System.out.println("suma\n"+suma.mostrarPolinomio());
+        
+          pol1.eliminarTermino(2);
+          System.out.println(pol1.mostrarPolinomio());
+         
+        PoliF1 producto;
+        producto = pol1.multiplicar(pol2);
+
+        System.out.println("producto: \n"+producto.mostrarPolinomio());
+
+    }
+}
+
