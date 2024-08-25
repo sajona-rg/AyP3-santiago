4590a00fa9b96952410ed5554e5bf92f8a4c96c5
Castacod <castajeffryd123@gmail.com>
2024-08-25 17:11:11 -0500

    esta es al actualizacion del insertar

--- PoliF1/PoliF1.java
+++ PoliF1/PoliF1.java
@@ -1,8 +1,6 @@
 package PoliF1;
 
-
-public class PoliF1 
-{
+public class PoliF1 {
     // atributos
     private int grado;
     private int[] vec;
@@ -14,52 +12,44 @@ public class PoliF1
         vec[1] = coef;
     }
 
-    public String mostrarPolinomio()
-    {
+    public String mostrarPolinomio() {
         String polinomio = "";
-        for (int i = 0; i < vec.length; i++) 
-        {
+        for (int i = 0; i < vec.length; i++) {
             polinomio += "[" + vec[i] + "] ";
         }
         return polinomio;
     }
 
     public void insertarTermino(int coef, int exp) {
-        if (exp <= grado)
-        {
+        if (exp <= grado) {
 
-            if (vec[vec.length - exp - 1] + coef == 0) 
-            {
+            if (vec[vec.length - exp - 1] + coef == 0) {
                 eliminarTermino(exp);
 
-            } else
-                {
-                    vec[vec.length - exp - 1] += coef;
+            } else {
+                vec[vec.length - exp - 1] += coef;
 
-                } 
-        }else 
-              {
-                int[] aux = new int[exp + 2];
+            }
+        } else {
+            int[] aux = new int[exp + 2];
 
-                for (int i = 0; i <= grado; i++) // grado = grado del primer vector.
-                  {
-                    aux[aux.length - i - 1] = vec[vec.length - i - 1];
-                  }
+            for (int i = 0; i <= grado; i++) // grado = grado del primer vector.
+            {
+                aux[aux.length - i - 1] = vec[vec.length - i - 1];
+            }
 
-                    grado = exp;
-                    aux[0] = grado;
-                    aux[1] = coef;
-                    vec = aux;
-             }
-        
+            grado = exp;
+            aux[0] = grado;
+            aux[1] = coef;
+            vec = aux;
+        }
+
     }
 
-    public void eliminarTermino(int exp)
-    {
+    public void eliminarTermino(int exp) {
         vec[grado + 1 - exp] = 0;
 
-        if (exp == grado) 
-        {
+        if (exp == grado) {
             reajustar();
         }
 
@@ -69,10 +59,8 @@ public class PoliF1
         boolean bandera = false;
         int conta = 0;
 
-        for (int i = 1; i < vec.length; i++) 
-        {
-            if (vec[i] != 0) 
-            {
+        for (int i = 1; i < vec.length; i++) {
+            if (vec[i] != 0) {
                 bandera = true;
                 break;
             }
@@ -95,39 +83,33 @@ public class PoliF1
         }
     }
 
-    public PoliF1 sumar(PoliF1 pol)
-    {
-        PoliF1 suma = new PoliF1(0,0);
+    public PoliF1 sumar(PoliF1 pol) {
+        PoliF1 suma = new PoliF1(0, 0);
 
-
-        for(int i = 1; i < vec.length; i++)
-        {
-            suma.insertarTermino(vec[i], grado-i+1);
+        for (int i = 1; i < vec.length; i++) {
+            suma.insertarTermino(vec[i], grado - i + 1);
         }
 
-        for(int i = 1; i < pol.vec.length; i++)
-        {
-            suma.insertarTermino(pol.vec[i], pol.grado-i+1);
+        for (int i = 1; i < pol.vec.length; i++) {
+            suma.insertarTermino(pol.vec[i], pol.grado - i + 1);
 
         }
 
         return suma;
     }
 
-    public PoliF1 multiplicar (PoliF1 pol)
-    {
-        PoliF1 producto = new PoliF1(grado + pol.grado,0);
+    public PoliF1 multiplicar(PoliF1 pol) {
+        PoliF1 producto = new PoliF1(grado + pol.grado, 0);
         int coef, exp;
-        for(int i = 1; i < vec.length; i++)
-        {
-            for(int j = 1; j < pol.vec.length; j++)
-            {
+        for (int i = 1; i < vec.length; i++) {
+            for (int j = 1; j < pol.vec.length; j++) {
                 coef = vec[i] * pol.vec[j];
-                exp = (grado-i+1) + (pol.grado-j+1);
+                exp = (grado - i + 1) + (pol.grado - j + 1);
 
                 producto.insertarTermino(coef, exp);
             }
         }
         return producto;
     }
+
 }
